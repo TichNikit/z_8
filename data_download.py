b5b6e8fe7429846de41103721e0e3f8b20745fa5
TichNikit <168722731+TichNikit@users.noreply.github.com>
2024-12-20 11:20:57 +0300

    data_download.py

--- data_download.py
+++ data_download.py
@@ -3,8 +3,19 @@ import pandas as pd
 
 
 def fetch_stock_data(ticker, start_date=None, end_date=None):
-    stock = yf.Ticker(ticker)
+    """
+    Получает исторические данные акций для заданного тикера.
 
+    Параметры:
+    ticker (str): Тикер акции (например, 'AAPL').
+    start_date (str): Дата начала в формате 'ГГГГ-ММ-ДД' (по умолчанию None).
+    end_date (str): Дата окончания в формате 'ГГГГ-ММ-ДД' (по умолчанию None).
+
+    Возвращает:
+    pd.DataFrame: Данные акций за указанный период или за последний месяц, если даты не указаны.
+    """
+
+    stock = yf.Ticker(ticker)
     try:
         if start_date and end_date:
             data = stock.history(start=start_date, end=end_date)
@@ -17,6 +28,17 @@ def fetch_stock_data(ticker, start_date=None, end_date=None):
 
 
 def add_moving_average(data, window_size=5):
+    """
+    Добавляет скользящую среднюю и стандартное отклонение в DataFrame с данными акций.
+
+    Параметры:
+    data (pd.DataFrame): DataFrame с данными акций.
+    window_size (int): Размер окна для расчета скользящей средней (по умолчанию 5).
+
+    Возвращает:
+    pd.DataFrame: Обновленный DataFrame с добавленными столбцами 'Moving_Average' и 'STD_Dev'.
+    """
+
     if 'Close' in data:
         data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
         data['STD_Dev'] = data['Close'].rolling(window=window_size).std()
@@ -24,6 +46,13 @@ def add_moving_average(data, window_size=5):
 
 
 def calculate_and_display_statistics(data):
+    """
+    Вычисляет и отображает среднюю цену закрытия и стандартное отклонение.
+
+    Параметры:
+    data (pd.DataFrame): DataFrame с данными акций.
+    """
+
     if not data.empty:
         average_price = data['Close'].mean()
         std_deviation = data['Close'].std()
@@ -34,6 +63,14 @@ def calculate_and_display_statistics(data):
 
 
 def notify_if_strong_fluctuations(data, threshold):
+    """
+    Уведомляет пользователя о сильных колебаниях цены акций.
+
+    Параметры:
+    data (pd.DataFrame): DataFrame с данными акций.
+    threshold (float): Порог в процентах для определения сильных колебаний.
+    """
+
     if 'Close' in data:
         initial_price = data['Close'].iloc[0]
         final_price = data['Close'].iloc[-1]
@@ -49,6 +86,14 @@ def notify_if_strong_fluctuations(data, threshold):
 
 
 def export_data_to_csv(data, file_name):
+    """
+    Экспортирует данные о ценах акций в CSV-файл.
+
+    Параметры:
+    data (pd.DataFrame): DataFrame с данными акций.
+    file_name (str): Имя выходного CSV-файла.
+    """
+
     try:
         data.to_csv(file_name, index=True)
         print(f"Данные экспортированы в файл: {file_name}")
@@ -57,6 +102,17 @@ def export_data_to_csv(data, file_name):
 
 
 def calculate_rsi(data, window=14):
+    """
+    Вычисляет индекс относительной силы (RSI) для данных акций.
+
+    Параметры:
+    data (pd.DataFrame): DataFrame с данными акций.
+    window (int): Размер окна для расчета RSI (по умолчанию 14).
+
+    Возвращает:
+    pd.DataFrame: Обновленный DataFrame с добавленным столбцом 'RSI'.
+    """
+
     if 'Close' in data:
         delta = data['Close'].diff()
         gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
@@ -67,6 +123,16 @@ def calculate_rsi(data, window=14):
 
 
 def calculate_macd(data):
+    """
+    Вычисляет MACD и сигнальную линию для данных акций.
+
+    Параметры:
+    data (pd.DataFrame): DataFrame с данными акций.
+
+    Возвращает:
+    pd.DataFrame: Обновленный DataFrame с добавленными столбцами 'MACD' и 'MACD_Signal'.
+    """
+
     if 'Close' in data:
         short_ema = data['Close'].ewm(span=12, adjust=False).mean()
         long_ema = data['Close'].ewm(span=26, adjust=False).mean()
